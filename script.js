153a6448d9419a9584372c8dc0b91eb0ec7d554a
EstelleR <roges.estelle@gmail.com>
2022-07-20 18:24:58 +0200

    script done - responsiveness to go

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const subscribe = (event) => {
   event.preventDefault();
 
   const mail = document.getElementById("mailAddress");
-  const errorMessage = document.getElementsByClassName(".errorMessage");
+  const errorMessage = document.getElementById("errorMessage");
 
   console.log(mail.value);
   console.log(errorMessage);
 
   const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
 
-//   if (!pattern.test(mail.value)) {
-//     mail.classList.add("badMail");
-//     errorMessage.style.display = "block";
-//   }
+  if (!pattern.test(mail.value)) {
+    mail.classList.add("badMail");
+    errorMessage.style.display = "block";
+  } else {
+    mail.classList.remove("badMail");
+    errorMessage.style.display = "none";
+    alert("Thanks for subscribing!");
+  }
 };
